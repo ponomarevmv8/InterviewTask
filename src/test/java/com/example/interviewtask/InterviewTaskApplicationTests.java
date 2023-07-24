68dab6f69972d46e29f06cb59de8797583bf9477
Maxim Ponomarev <ponomarevmv8@yandex.ru>
2023-07-24 15:57:19 +0300

    init commit

---
+++ src/test/java/com/example/interviewtask/InterviewTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.interviewtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
